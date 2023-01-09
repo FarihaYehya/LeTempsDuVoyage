9ba047190e1bdd62b6891983ade59451a8a50a3c
Lauryn <lauryn.martin333@gmail.com>
2023-01-09 12:24:46 +0100

    ajout des méthodes et lien entre spring et html/css

--- src/main/java/com/letempsduvoyage/LeTempsDuVoyage/controllers/MainWebController.java
+++ src/main/java/com/letempsduvoyage/LeTempsDuVoyage/controllers/MainWebController.java
@@ -1,9 +1,40 @@
 package com.letempsduvoyage.LeTempsDuVoyage.controllers;
 
+import com.letempsduvoyage.LeTempsDuVoyage.beans.Villes;
+import com.letempsduvoyage.LeTempsDuVoyage.repositories.VillesRepositoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/API")
 public class MainWebController {
+
+    @Autowired
+    private VillesRepositoryInterface villesRepositoryInterface;
+
+    @GetMapping("/ajoutVilleEnBase")
+    public String ajoutVilleEnBase() {
+
+        Villes ville1 = new Villes("Kenya");
+        Villes ville2 = new Villes("Copacabana");
+        Villes ville3 = new Villes("Dubaï");
+        Villes ville4 = new Villes("Istrie");
+
+        villesRepositoryInterface.save(ville1);
+        villesRepositoryInterface.save(ville2);
+        villesRepositoryInterface.save(ville3);
+        villesRepositoryInterface.save(ville4);
+
+        return ("OK");
+    }
+
+    @GetMapping("/getAllVilles")
+    public List<Villes> getAllVille() {
+        List<Villes> villes = villesRepositoryInterface.findAll();
+        return villes;
+    }
 }
---
+++ src/main/resources/static/js.TempsDuVoyage/index.js
@@ -0,0 +1,13 @@
+
+//Afficher une ville//
+
+$.ajax({
+  type: "GET",
+  headers: { "Content-Type": "application/json" },
+  url: "http://localhost:8080/API/getAllVilles",
+  success: function (resultat) {
+    alert("L'API m'a retourné : "+resultat);
+    $("#Kenya").html(resultat[0].nom.replace(/[""]+/g, ''));
+  }
+  });
+
